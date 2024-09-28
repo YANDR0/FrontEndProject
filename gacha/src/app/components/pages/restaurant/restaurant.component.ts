e46f85eff663ebb5679be33246db7da94ed06ab5
Yael <yael.rodriguez@iteso.mx>
2024-09-28 17:13:42 -0600

    restuno medioacabasionado

--- gacha/src/app/components/pages/restaurant/restaurant.component.ts
+++ gacha/src/app/components/pages/restaurant/restaurant.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../../../modules/material/material.module';
+
 
 @Component({
   selector: 'app-restaurant',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './restaurant.component.html',
   styleUrl: './restaurant.component.scss'
 })
