3185ab3858ba4c62faf3adfa63eb1c6b8de1dcaf
Yael <yael.rodriguez@iteso.mx>
2024-10-22 15:43:56 -0600

    Olvide actualizar http

--- gacha/src/app/services/http-service.service.ts
+++ gacha/src/app/services/http-service.service.ts
@@ -9,4 +9,25 @@ export class HttpServiceService {
 
   generalUrl = 'http://localhost:4200/';
   constructor(private httpClient: HttpClient) {}
+
+  getHttp(route: String){
+    const url = this.generalUrl + route;
+    return this.httpClient.get<any>(url);
+  }
+
+  postHttp(route: String, body: any){
+    const url = this.generalUrl + route;
+    return this.httpClient.post<any>(url, body);
+  }
+
+  putHttp(route: String, body: any){
+    const url = this.generalUrl + route;
+    return this.httpClient.put<any>(url, body);
+  }
+
+  deleteHttp(route: String, body: any){
+    const url = this.generalUrl + route;
+    return this.httpClient.delete<any>(url, body);
+  }
+
 }
