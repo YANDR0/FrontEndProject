6222188cf483a7dc0e9d6deac579ce8f2b37b03c
Yael <yael.rodriguez@iteso.mx>
2024-10-23 17:20:49 -0600

    Login y sabe porque no

--- gacha/src/app/components/pages/login/login.component.ts
+++ gacha/src/app/components/pages/login/login.component.ts
@@ -3,6 +3,9 @@ import { MaterialModule } from '../../../modules/material/material.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
+import { LoginService } from '../../../services/login.service';
+import { AuthService } from '../../../services/auth.service';
+import { Users } from '../../../types/users';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +17,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class LoginComponent {
   form: FormGroup;
 
-  constructor(formBuilder: FormBuilder, private router: Router, private activatedRoute: ActivatedRoute) {
+  constructor(formBuilder: FormBuilder, private router: Router, private activatedRoute: ActivatedRoute, private loginService: LoginService, private authService: AuthService) {
     this.form = formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
@@ -23,9 +26,23 @@ export class LoginComponent {
 
   login() {
     if (this.form.valid) {
-      console.log('Login successful');
-      //TODO calls login service
-      this.router.navigate(['../home'], { relativeTo: this.activatedRoute });
+      const userData: Users = { 
+        email: this.form.value.email, 
+        password: this.form.value.password
+      }
+
+      this.loginService.login(userData).subscribe({
+        next: (response) => {
+          console.log('Login successful');
+          this.authService.saveToken(response.token)
+          this.router.navigate(['../home'], { relativeTo: this.activatedRoute });
+        },
+        error: (error) => {
+          console.error('Error during login:', error);
+          alert('Login failed. Please try again.');
+          this.form.reset();
+        } 
+      })
     }
     else {
       alert('Please fill in all fields');
@@ -33,7 +50,7 @@ export class LoginComponent {
     }
   }
 
-  navRegister() {
-    this.router.navigate(['../register'], { relativeTo: this.activatedRoute });
+  navLogin() {
+    this.router.navigate(['../login'], { relativeTo: this.activatedRoute });
   }
 }
